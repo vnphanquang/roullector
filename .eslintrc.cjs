a42c6dfe0d057c56693a2041472418c80b605326
Quang Phan <vnphanquang@gmail.com>
2022-03-07 09:13:27 +0700

    chore: eslint & tsconfig

---
+++ .eslintrc.cjs
@@ -0,0 +1,95 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript"
+  ],
+  plugins: ["import", "@typescript-eslint", "modules-newline", "prettier"],
+  parser: "@typescript-eslint/parser",
+  rules: {
+    semi: "off",
+    "@typescript-eslint/semi": ["error"],
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      {
+        prefer: "type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/member-delimiter-style": [
+      "error",
+      {
+        multiline: {
+          delimiter: "semi",
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: "semi",
+          requireLast: true,
+        },
+      },
+    ],
+    "object-curly-newline": [
+      "error",
+      {
+        ObjectExpression: {
+          consistent: true,
+        },
+        ObjectPattern: {
+          consistent: true,
+        },
+        ImportDeclaration: {
+          minProperties: 2,
+          multiline: true,
+        },
+        ExportDeclaration: {
+          minProperties: 2,
+          multiline: true,
+        },
+      },
+    ],
+    "import/prefer-default-export": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "js": "never",
+        "jsx": "never",
+        "ts": "never",
+        "tsx": "never"
+      }
+    ],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "unknown",
+        ],
+        pathGroups: [],
+        "newlines-between": "always",
+        pathGroupsExcludedImportTypes: [],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        warnOnUnassignedImports: true,
+      },
+    ],
+  },
+  env: {
+    node: true,
+  },
+  overrides: [],
+};
