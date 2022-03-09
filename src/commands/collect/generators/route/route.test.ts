11ca8b07e7100383195f45ffe21779cd9a0e7ae7
Quang Phan <vnphanquang@gmail.com>
2022-03-09 16:01:43 +0700

    chore(collect.route): split route & source test suite

--- src/commands/collect/generators/route/route.test.ts
+++ src/commands/collect/generators/route/route.test.ts
@@ -8,7 +8,6 @@ import {
 import { defaultCollectOptions } from '$commands/collect/collect.constants';
 import type { CollectOptions } from '$commands/collect/collect.types';
 import { generateRouteUtil } from '$commands/collect/generators/route';
-import { route } from '$commands/collect/generators/route/source';
 
 jest.mock('fs', () => {
   const fs = jest.requireActual('fs');
@@ -18,29 +17,6 @@ jest.mock('fs', () => {
     [sourcePath]: sourceOfRouteUtil,
   }, __dirname);
   return memfs;
-});
-
-describe('map path with route util', () => {
-  test('static path: return same', () => {
-    const path = '/a/literal/static/path';
-    expect(route(path)).toBe(path);
-  });
-
-  test('dynamic path: argument is correctly injected', () => {
-    const parent = '/a-path-with';
-    const id = 'a-unique-id-123-124';
-    expect(route(`${parent}/[arg]`, id)).toBe(`${parent}/${id}`);
-  });
-
-  test('dynamic path: multiple arguments are injected as specified', () => {
-    const path = '/a-path-with/[arg_snake]/and/[arg-kebab]/and/[argCamel]';
-    const argSnake = 'snake';
-    const argKebab = 'kebab';
-    const argCamel = 'camel';
-
-    const routed = route(path, argSnake, argKebab, argCamel);
-    expect(routed).toBe(`/a-path-with/${argSnake}/and/${argKebab}/and/${argCamel}`);
-  });
 });
 
 describe('generate route utils:', () => {
