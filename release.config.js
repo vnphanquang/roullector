ec12f8035332a2e6db5110064131b992ec75f980
Quang Phan <vnphanquang@gmail.com>
2022-03-10 11:45:05 +0700

    chore(github.actions): add semantic release workflow

---
+++ release.config.js
@@ -0,0 +1,39 @@
+/** @type {import('semantic-release').Options} */
+const options = {
+  ci: true,
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'main',
+    'next',
+    'next-major',
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: [
+          'CHANGELOG.md',
+          'package.json',
+          'package-lock.json',
+          'npm-shrinkwrap.json',
+        ],
+        message: `chore(release): <%= nextRelease.version %> - <%= new Date().toLocaleDateString('en-US', {year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' }) %> [skip ci]`,
+      },
+    ],
+    '@semantic-release/github',
+  ],
+  dryRun: true,
+};
+
+module.exports = options;
